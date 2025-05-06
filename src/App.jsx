71d4df267676a6ab3b8e717a4e16ccc2a3e2351e
kushaan1 <kushaan.nagpal02@gmail.com>
2025-05-06 22:09:15 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import DigiClock from './digiClock'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <DigiClock />
+    </div>
+  );
+}
+
+export default App
